aa5e2d547c1b624cbe4417018655fbc27c78c30b
Анна <anna_guzii@ukr.net>
2022-01-19 19:28:44 +0200

    version 2

--- src/main/java/FigureName.java
+++ src/main/java/FigureName.java
@@ -1,5 +1,17 @@
 public class FigureName{
-    String print(Shape shape){
+    String printPoint(Shape shape){
       return shape.shapeName(new double[]{1.0D}, 0.0D);
+    }
+    String printLine(Shape shape){
+        return shape.shapeName(new double[]{1.0D,2.0D}, 0.0D);
+    }
+    String printTriangle(Shape shape){
+        return shape.shapeName(new double[]{1.0D,2.0D,3.0D}, 0.0D);
+    }
+    String printQuadRect(Shape shape){
+        return shape.shapeName(new double[]{2.0D,2.0D,2.0D,2.0D}, 0.0D);
+    }
+    String printCircle(Shape shape){
+        return shape.shapeName(new double[]{}, 1.0D);
     }
 }
--- src/main/java/ShapeTest.java
+++ src/main/java/ShapeTest.java
@@ -1,6 +1,10 @@
 public class ShapeTest {
 
       public static void main(String[] args) {
-          System.out.println(new FigureName().print(new Point()));
+          System.out.println(new FigureName().printPoint(new Point()));
+          System.out.println(new FigureName().printLine(new Line()));
+          System.out.println(new FigureName().printTriangle(new Triangle()));
+          System.out.println(new FigureName().printQuadRect(new QuadRect()));
+          System.out.println(new FigureName().printCircle(new Circle()));
     }
 }
